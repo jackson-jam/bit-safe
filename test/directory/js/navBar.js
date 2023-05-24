a5391fb8322fe14b99681a22165da8fa6679a670
jakeson <83272722+jackson-jam@users.noreply.github.com>
2023-05-24 09:06:09 +0800

    init

--- test/directory/js/navBar.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-    $('.nav-title').click(function() {
-        $(this).siblings('.nav-content').stop().toggle(500).parent().siblings('.nav-menu').children('.nav-content').hide(500);
-    })
-})
